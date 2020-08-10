7c5a659fd985a8282ab380d4fba73c0ddabc5db6
locky-mac <lpye886@aucklanduni.ac.nz>
2020-08-10 17:31:02 +1200

    Testing lighting effects

---
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+    public float moveSpeed = 5;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.right * moveSpeed * Input.GetAxis("Horizontal") * Time.deltaTime);
+        transform.Translate(Vector3.up * moveSpeed * Input.GetAxis("Vertical") * Time.deltaTime);
+    }
+}
