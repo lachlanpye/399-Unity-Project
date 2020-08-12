c3b9de2f6c5e5e6998f4910312127aed030a12b7
lachlanpye <pyelachlan@gmail.com>
2020-08-12 20:03:31 +1200

    Experimenting with player movement

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -4,12 +4,53 @@ using UnityEngine;
 
 public class PlayerBehaviour : MonoBehaviour
 {
-    public float moveSpeed = 5;
+    [Tooltip("Number of pixels per second to move.")]
+    public float moveSpeed;
+
+    private float distance;
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.right * moveSpeed * Input.GetAxis("Horizontal") * Time.deltaTime);
-        transform.Translate(Vector3.up * moveSpeed * Input.GetAxis("Vertical") * Time.deltaTime);
+        distance = moveSpeed * 16 * Time.deltaTime;
+
+        if (Input.GetAxis("Horizontal") > 0.5)
+        {
+            transform.Translate(new Vector3(distance, 0, 0));
+        }
+        else if (Input.GetAxis("Horizontal") < -0.5)
+        {
+            transform.Translate(new Vector3(-distance, 0, 0));
+        }
+        else
+        {
+            RoundPositionX();
+        }
+
+        if (Input.GetAxis("Vertical") > 0.5)
+        {
+            transform.Translate(new Vector3(0, distance, 0));
+        }
+        else if (Input.GetAxis("Vertical") < -0.5)
+        {
+            transform.Translate(new Vector3(0, -distance, 0));
+        }
+        else
+        {
+            RoundPositionY();
+        }
+    } 
+
+    private void RoundPositionX()
+    {
+        Vector3 pos = transform.position;
+        pos.x = (float) (Mathf.Round(pos.x));
+        transform.position = pos;
+    }
+    private void RoundPositionY()
+    {
+        Vector3 pos = transform.position;
+        pos.y = (float)(Mathf.Round(pos.y));
+        transform.position = pos;
     }
 }
