7a8f79141d4a490bc60972a9dfbf2379e269bc86
fcampbell7 <54756263+fcampbell7@users.noreply.github.com>
2020-08-20 23:55:40 +1200

    Added basic collision detection and player sprite
    Currently has bugs which need hashing out so this can actually be tested...

---
+++ Assets/Scripts/Environment/House/change_room.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class change_room : MonoBehaviour
+{
+    public void OnTriggerEnter2D(Collider2D player){
+        Debug.Log("Player detected.")
+    }
+}
