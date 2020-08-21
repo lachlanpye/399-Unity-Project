98002db113e01e52f55a07aaf4f5f55841aab984
fcampbell7 <54756263+fcampbell7@users.noreply.github.com>
2020-08-21 13:25:56 +1200

    Added first room change hit box
    Teleports player and camera to the left most room

--- Assets/Scripts/Environment/House/change_room.cs
+++ Assets/Scripts/Environment/House/change_room.cs
@@ -2,9 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//This script will teleport the player, the camera to desired room. Will also change base layer colour.
+
 public class change_room : MonoBehaviour
 {
+    public GameObject Player;
+    public GameObject Camera;
+
     public void OnTriggerEnter2D(Collider2D other){
         Debug.Log("Player detected.");
+        Player.transform.position = new Vector3(-12,-1.17f,0);
+        Camera.transform.position = new Vector3(-17.51f,-1,-10);
     }
 }
