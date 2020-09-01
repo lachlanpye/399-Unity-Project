f30fb93b524f04edf182f9557cc2d38d0c2397ca
lachlanpye <pyelachlan@gmail.com>
2020-09-02 11:55:08 +1200

    Added day and night object switching

---
+++ Assets/Scripts/EnvironmentScripts/DayOrNightObjects.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayOrNightObjects : MonoBehaviour
+{
+    public GameObject dayObjects;
+    public GameObject nightObjects;
+
+    public bool currentlyDay;
+
+    void Update()
+    {
+        if (currentlyDay == true)
+        {
+            ChangeToDay();
+        }
+        if (currentlyDay == false)
+        {
+            ChangeToNight();
+        }
+    }
+
+    public void ChangeToDay()
+    {
+        dayObjects.SetActive(true);
+        nightObjects.SetActive(false);
+    }
+
+    public void ChangeToNight()
+    {
+        dayObjects.SetActive(false);
+        nightObjects.SetActive(true);
+    }
+}
