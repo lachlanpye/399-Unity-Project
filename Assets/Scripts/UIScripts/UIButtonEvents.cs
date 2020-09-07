ab7344a8e5660d1a950d1cbfa85c5a3dceae007d
lachlanpye <pyelachlan@gmail.com>
2020-09-07 12:59:37 +1200

    Added UI buttons script

---
+++ Assets/Scripts/UIScripts/UIButtonEvents.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIButtonEvents : MonoBehaviour
+{
+    public GameObject gameController;
+
+    private WorldControl worldControl;
+
+    void Start()
+    {
+        worldControl = gameController.GetComponent<WorldControl>();
+    }
+
+    void Update()
+    {
+        if (Input.GetAxis("Pause") > 0)
+        {
+            worldControl.Pause();
+        }
+    }
+
+    public void Advance()
+    {
+        worldControl.GetNextLine();
+    }
+}
