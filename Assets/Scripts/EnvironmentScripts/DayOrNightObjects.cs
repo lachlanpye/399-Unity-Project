5fae38d9b3cb3ef48cacad9066b7fb0cf870513d
lachlanpye <pyelachlan@gmail.com>
2020-09-09 14:06:52 +1200

    Added script for color switching to area zone triggers

--- Assets/Scripts/EnvironmentScripts/DayOrNightObjects.cs
+++ Assets/Scripts/EnvironmentScripts/DayOrNightObjects.cs
@@ -1,102 +1,144 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.Tilemaps;
 
-public class DayOrNightObjects : MonoBehaviour
+namespace Enviroment
 {
-    public GameObject dayObjects;
-    public GameObject nightObjects;
-
-    public bool currentlyDay;
-
-    [System.Serializable]
-    public struct LightingColor
+    public class DayOrNightObjects : MonoBehaviour
     {
-        public GameObject sceneObject;
-        public Color dayColor;
-        public Color nightColor;
+        public GameObject dayObjects;
+        public GameObject nightObjects;
 
-        [HideInInspector]
-        public Component renderer;
-    }
-    [Space]
-    public LightingColor[] dayOrNightLightingColors;
+        public bool currentlyDay;
 
-    void Start()
-    {
-        for (int i = 0; i < dayOrNightLightingColors.Length; i++)
+        [System.Serializable]
+        public struct LightingColor
         {
-            if (dayOrNightLightingColors[i].sceneObject.GetComponent<SpriteRenderer>() != null)
+            public GameObject sceneObject;
+            public Color dayColor;
+            public Color nightColor;
+
+            [HideInInspector]
+            public Component renderer;
+        }
+        [Space]
+        public LightingColor[] dayOrNightLightingColors;
+
+        void Start()
+        {
+            for (int i = 0; i < dayOrNightLightingColors.Length; i++)
             {
-                dayOrNightLightingColors[i].renderer = dayOrNightLightingColors[i].sceneObject.GetComponent<SpriteRenderer>();
+                if (dayOrNightLightingColors[i].sceneObject.GetComponent<SpriteRenderer>() != null)
+                {
+                    dayOrNightLightingColors[i].renderer = dayOrNightLightingColors[i].sceneObject.GetComponent<SpriteRenderer>();
+                }
+                else if (dayOrNightLightingColors[i].sceneObject.GetComponent<Tilemap>() != null)
+                {
+                    dayOrNightLightingColors[i].renderer = dayOrNightLightingColors[i].sceneObject.GetComponent<Tilemap>();
+                }
+                else
+                {
+                    dayOrNightLightingColors[i].renderer = null;
+                }
             }
-            else if (dayOrNightLightingColors[i].sceneObject.GetComponent<Tilemap>() != null)
+        }
+
+        void Update()
+        {
+            if (currentlyDay == true)
             {
-                dayOrNightLightingColors[i].renderer = dayOrNightLightingColors[i].sceneObject.GetComponent<Tilemap>();
+                ChangeToDay();
+            }
+            if (currentlyDay == false)
+            {
+                ChangeToNight();
+            }
+        }
+
+        public void ChangeToDay()
+        {
+            dayObjects.SetActive(true);
+            nightObjects.SetActive(false);
+
+            for (int i = 0; i < dayOrNightLightingColors.Length; i++)
+            {
+                if (dayOrNightLightingColors[i].renderer != null)
+                {
+                    if (dayOrNightLightingColors[i].renderer is SpriteRenderer)
+                    {
+                        SpriteRenderer spriteRenderer = (SpriteRenderer)dayOrNightLightingColors[i].renderer;
+                        spriteRenderer.color = dayOrNightLightingColors[i].dayColor;
+                    }
+                    else if (dayOrNightLightingColors[i].renderer is Tilemap)
+                    {
+                        Tilemap tilemapRenderer = (Tilemap)dayOrNightLightingColors[i].renderer;
+                        tilemapRenderer.color = dayOrNightLightingColors[i].dayColor;
+                    }
+                }
+            }
+        }
+
+        public void ChangeToNight()
+        {
+            dayObjects.SetActive(false);
+            nightObjects.SetActive(true);
+
+            for (int i = 0; i < dayOrNightLightingColors.Length; i++)
+            {
+                if (dayOrNightLightingColors[i].renderer != null)
+                {
+                    if (dayOrNightLightingColors[i].renderer is SpriteRenderer)
+                    {
+                        SpriteRenderer spriteRenderer = (SpriteRenderer)dayOrNightLightingColors[i].renderer;
+                        spriteRenderer.color = dayOrNightLightingColors[i].nightColor;
+                    }
+                    else if (dayOrNightLightingColors[i].renderer is Tilemap)
+                    {
+                        Tilemap tilemapRenderer = (Tilemap)dayOrNightLightingColors[i].renderer;
+                        tilemapRenderer.color = dayOrNightLightingColors[i].nightColor;
+                    }
+                }
+            }
+        }
+
+        public void UpdateColorValues(LightingColor[] newLightingColors)
+        {
+            for (int i = 0; i < newLightingColors.Length; i++)
+            {
+                if (newLightingColors[i].sceneObject.GetComponent<SpriteRenderer>() != null)
+                {
+                    newLightingColors[i].renderer = newLightingColors[i].sceneObject.GetComponent<SpriteRenderer>();
+                }
+                else if (newLightingColors[i].sceneObject.GetComponent<Tilemap>() != null)
+                {
+                    newLightingColors[i].renderer = newLightingColors[i].sceneObject.GetComponent<Tilemap>();
+                }
+                else
+                {
+                    newLightingColors[i].renderer = null;
+                }
+            }
+            for (int i = 0; i < newLightingColors.Length; i++)
+            {
+                if (newLightingColors[i].renderer != null)
+                {
+                    for (int j = 0; j < dayOrNightLightingColors.Length; j++)
+                    {
+                        if (GameObject.ReferenceEquals(newLightingColors[i].sceneObject, dayOrNightLightingColors[j].sceneObject))
+                        {
+                            dayOrNightLightingColors[j].dayColor = newLightingColors[i].dayColor;
+                            dayOrNightLightingColors[j].nightColor = newLightingColors[i].nightColor;
+                        }
+                    }
+                }
+            }
+
+            if (currentlyDay)
+            {
+                ChangeToDay();
             }
             else
             {
-                dayOrNightLightingColors[i].renderer = null;
-            }
-        }
-    }
-
-    void Update()
-    {
-        if (currentlyDay == true)
-        {
-            ChangeToDay();
-        }
-        if (currentlyDay == false)
-        {
-            ChangeToNight();
-        }
-    }
-
-    public void ChangeToDay()
-    {
-        dayObjects.SetActive(true);
-        nightObjects.SetActive(false);
-
-        for (int i = 0; i < dayOrNightLightingColors.Length; i++)
-        {
-            if (dayOrNightLightingColors[i].renderer != null)
-            {
-                if (dayOrNightLightingColors[i].renderer is SpriteRenderer)
-                {
-                    SpriteRenderer spriteRenderer = (SpriteRenderer) dayOrNightLightingColors[i].renderer;
-                    spriteRenderer.color = dayOrNightLightingColors[i].dayColor;
-                }
-                else if (dayOrNightLightingColors[i].renderer is Tilemap)
-                {
-                    Tilemap tilemapRenderer = (Tilemap) dayOrNightLightingColors[i].renderer;
-                    tilemapRenderer.color = dayOrNightLightingColors[i].dayColor;
-                }
-            }
-        }
-    }
-
-    public void ChangeToNight()
-    {
-        dayObjects.SetActive(false);
-        nightObjects.SetActive(true);
-
-        for (int i = 0; i < dayOrNightLightingColors.Length; i++)
-        {
-            if (dayOrNightLightingColors[i].renderer != null)
-            {
-                if (dayOrNightLightingColors[i].renderer is SpriteRenderer)
-                {
-                    SpriteRenderer spriteRenderer = (SpriteRenderer)dayOrNightLightingColors[i].renderer;
-                    spriteRenderer.color = dayOrNightLightingColors[i].nightColor;
-                }
-                else if (dayOrNightLightingColors[i].renderer is Tilemap)
-                {
-                    Tilemap tilemapRenderer = (Tilemap)dayOrNightLightingColors[i].renderer;
-                    tilemapRenderer.color = dayOrNightLightingColors[i].nightColor;
-                }
+                ChangeToNight();
             }
         }
     }
---
+++ Assets/Scripts/EnvironmentScripts/ZoneChangeObjectColor.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace Enviroment
+{
+    public class ZoneChangeObjectColor : MonoBehaviour
+    {
+        public DayOrNightObjects.LightingColor[] newLightingColors;
+
+        [System.Serializable]
+        public class ChangeColorEvent : UnityEvent<DayOrNightObjects.LightingColor[]> { };
+        [SerializeField]
+        [Header("Setting up object color changes:", order = 0)]
+        [Space(-10, order = 1)]
+        [Header("1) Use 'SceneObjects' as the GameObject.", order = 2)]
+        [Space(-10, order = 3)]
+        [Header("2) Select DayOrNightObjects > MoveScenes as the function.", order = 4)]
+        [Space(-10, order = 5)]
+        [Header("3) Set up the newLightingColors array as you want the objects to appear.", order = 6)]
+        [Space(-10, order = 7)]
+        [Header("Upon entering this trigger, the colors will be updated.", order = 8)]
+        [Space(order = 9)]
+        public ChangeColorEvent changeColorEvent;
+
+        void Awake()
+        {
+            if (changeColorEvent == null)
+            {
+                changeColorEvent = new ChangeColorEvent();
+            }
+        }
+
+        void OnTriggerEnter2D(Collider2D col)
+        {
+            if (col.gameObject.tag == "Player")
+            {
+                changeColorEvent.Invoke(newLightingColors);
+            }
+        }
+    }
+}
