da5aacc85f20de8ffd6448afead18c5f99e8aaf6
jaunzo <jabaunzo@yahoo.com>
2020-10-08 01:02:52 +1300

    Added forest bgm, altered father dialogue and door slam audio clip
    Altered father dialogue and door slam sound effect for better sound when playing with or without headphones

---
+++ Assets/Scripts/Audio/ForestAudio.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForestAudio : MonoBehaviour
+{
+    #pragma warning disable 0649
+    [SerializeField] private AudioClip townBGM;
+    #pragma warning restore 0649
+
+
+    private void Awake()
+    {
+        AudioManager.publicInstance.FadeInBGM(townBGM);
+    }
+
+}
