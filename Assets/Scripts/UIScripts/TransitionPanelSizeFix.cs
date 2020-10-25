fe9a5e467a62150c830f92e62efca16c1d7629e9
lachlanpye <pyelachlan@gmail.com>
2020-10-26 12:55:00 +1300

    Potentially fixed transition panel size issue

--- Assets/Scripts/UIScripts/TransitionPanelSizeFix.cs
+++ Assets/Scripts/UIScripts/TransitionPanelSizeFix.cs
@@ -3,19 +3,38 @@
 public class TransitionPanelSizeFix : MonoBehaviour
 {
     private RectTransform rectTransform;
+
     private Vector2 smallSize;
     private Vector2 largeSize;
+
+    private int widthLimit;
+    private int heightLimit;
 
     void Start()
     {
         rectTransform = transform as RectTransform;
-        smallSize = new Vector2(961, 721);
-        largeSize = new Vector2(481, 361);
+
+        if (Application.platform == RuntimePlatform.OSXEditor || Application.platform == RuntimePlatform.OSXPlayer)
+        {
+            smallSize = new Vector2(1441, 1081);
+            largeSize = new Vector2(481, 361);
+
+            widthLimit = 1440;
+            heightLimit = 1080;
+        }
+        else if (Application.platform == RuntimePlatform.WindowsEditor || Application.platform == RuntimePlatform.WindowsPlayer)
+        {
+            smallSize = new Vector2(961, 721);
+            largeSize = new Vector2(481, 361);
+
+            widthLimit = 960;
+            heightLimit = 720;
+        }
     }
 
     void Update()
     {
-        if (Screen.width >= 960 && Screen.height >= 720)
+        if (Screen.width >= widthLimit && Screen.height >= heightLimit)
         {
             rectTransform.sizeDelta = smallSize;
         }
